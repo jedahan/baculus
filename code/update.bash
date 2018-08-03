910f387ef451d2d05d4763f32be7c11e2af220fa
Jonathan Dahan <hi@jonathan.is>
2018-08-03 16:38:45 -0400

    self-assign adhoc ip

--- code/update.bash
+++ code/update.bash
@@ -103,6 +103,16 @@ sudo ifconfig $mesh_dev down
 sudo iw $mesh_dev set type ibss
 sudo ifconfig $mesh_dev up
 sudo iw dev $mesh_dev ibss join ${mesh_name} 2412 HT40+
+
+local suffix=5
+local selfassigned=$(ip addr show $mesh_dev | awk "/169/ {print \$2}")
+
+if [[ $HOST == 'baculusA' ]]; then suffix=10; fi
+if [[ $HOST == 'baculusB' ]]; then suffix=11; fi
+if [[ $HOST == 'baculusC' ]]; then suffix=12; fi
+
+sudo ip addr del $selfassigned dev $mesh_dev
+sudo ip addr add 10.0.17.${suffix} dev $mesh_dev
 ' > $HOME/adhoc.sh
   }
   bash $HOME/adhoc.sh
