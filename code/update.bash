d438350e8fb8e3a9ac150c47df05b9b5c70240ae
Jonathan Dahan <hi@jonathan.is>
2018-08-11 14:45:57 -0400

    export the right language to fix jekyll build

--- code/update.bash
+++ code/update.bash
@@ -43,7 +43,9 @@ require() {
 }
 
 raspi_config() {
-  test "$LANG" = 'en_US.UTF-8' || sudo raspi-config nonint do_change_locale "$_"
+  test "$LANG" = 'en_US.UTF-8' || sudo raspi-config nonint do_change_locale 'en_US.UTF-8'
+  export LANG=en_US.UTF-8
+  export LANGUAGE=$LANG
   sudo raspi-config nonint do_configure_keyboard us
   sudo raspi-config nonint do_wifi_country US
   sudo raspi-config nonint do_ssh 0
